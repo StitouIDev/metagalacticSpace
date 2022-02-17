4f9cd7ca129f471d3f7c28cb8013ce9414cd3e3b
Hamza <yourgitemail@example.com>
2022-02-17 20:15:49 +0100

    metagalctic

---
+++ metagalcticSpace/PlanetsCell.swift
@@ -0,0 +1,24 @@
+//
+//  PlanetsCell.swift
+//  metagalcticSpace
+//
+//  Created by Hamza on 2/13/22.
+//  Copyright © 2022 Hamza. All rights reserved.
+//
+
+import UIKit
+
+class PlanetsCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
